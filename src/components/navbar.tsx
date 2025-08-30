29f934f8933d937eadd8432d29695d9e4a460cbf
deep10dr <deepakofficial81@gmail.com>
2025-08-30 21:22:05 +0530

    added the profile image

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,20 +1,34 @@
 import Image from "next/image";
-
+import { getUserWithExpiry } from "@/utils/storage";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
 export default function NavBar() {
+  const [name, setName] = useState<string | undefined>("");
+  const router = useRouter();
+  useEffect(() => {
+    const data: string | undefined = getUserWithExpiry("user")?.name;
+    setName(data);
+  }, []);
   return (
     <header className="flex justify-between items-center w-full px-6 py-4 shadow-md ">
-      {/* Logo / Title */}
       <h1 className="md:text-4xl text-3xl font-extrabold text-white tracking-wide">
         Ephemera
       </h1>
 
-      {/* Avatar */}
-      <div className="relative w-10 h-10 rounded-full overflow-hidden border-2 border-white shadow-md hover:scale-110 transition-transform duration-300 cursor-pointer">
+      <div
+        className="relative w-10 h-10 rounded-full overflow-hidden border-2 border-white shadow-md hover:scale-110 transition-transform duration-300 cursor-pointer"
+        onClick={() => {
+          router.push("/user");
+        }}
+      >
         <Image
-          src="https://cdn-icons-png.flaticon.com/128/1999/1999625.png"
+          src={`https://ui-avatars.com/api/?name=${encodeURIComponent(
+            name ? name : "unkown"
+          )}&background=0D8ABC&color=fff&format=png`}
           fill
           alt="avatar"
           quality={100}
+          unoptimized
         />
       </div>
     </header>
